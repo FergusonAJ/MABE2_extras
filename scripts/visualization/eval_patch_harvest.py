757cc3c8eee5b40fe655755fe2f0c4599c96313b
Austin Ferguson <ferguson.austin.j@gmail.com>
2023-01-23 15:46:10 -0500

    Support metadata and image output

--- scripts/visualization/eval_patch_harvest.py
+++ scripts/visualization/eval_patch_harvest.py
@@ -32,6 +32,28 @@ class Agent:
             self.row_idx += 1
         elif self.facing == 7: # West
             self.col_idx -= 1
+    
+    def move_back(self):
+        if self.facing == 0:   # Northwest
+            self.col_idx += 1
+            self.row_idx += 1
+        elif self.facing == 1: # North
+            self.row_idx += 1
+        elif self.facing == 2: # Northeast
+            self.col_idx -= 1
+            self.row_idx += 1
+        elif self.facing == 3: # East
+            self.col_idx -= 1
+        elif self.facing == 4: # Southeast
+            self.col_idx -= 1
+            self.row_idx -= 1
+        elif self.facing == 5: # South
+            self.row_idx -= 1
+        elif self.facing == 6: # Southwest
+            self.col_idx += 1
+            self.row_idx -= 1
+        elif self.facing == 7: # West
+            self.col_idx += 1
 
     def rotate_right(self):
         self.facing += 1
@@ -46,18 +68,52 @@ class Agent:
 class PatchHarvestAnimator:
     def __init__(self, map_filename):
         self.map_filename = map_filename
+        self.metadata = {}
         self.load_map()
+        self.validate_metadata()
         self.setup_color_map()
         #self.agent = Agent(self.get_map_width() // 2, self.get_map_height() // 2, 1)
         #self.agent = Agent(34, 25, 3)
-        self.agent = Agent(40, 25, 3)
+        start_y = int(self.metadata['height']) - int(self.metadata['start_y']) - 1
+        self.agent = Agent(
+                int(self.metadata['start_x']),
+                start_y,
+                int(self.metadata['start_facing']))
         self.consume()
+        self.trace_points = []
+        self.add_trace()
+
+    def read_metadata(self, s):
+        print('metadata:', s)
+        s = s[1:] # Remove leading $
+        line_parts = s.split('=')
+        self.metadata[line_parts[0].strip()] = line_parts[1].strip()
+    
+    def validate_metadata(self):
+        if 'start_facing' not in self.metadata:
+            print('Error! Map\'s metadata must include "start_facing"')
+            exit()
+        if 'start_x' not in self.metadata:
+            print('Error! Map\'s metadata must include "start_x"')
+            exit()
+        if 'start_y' not in self.metadata:
+            print('Error! Map\'s metadata must include "start_y"')
+            exit()
+        if 'width' not in self.metadata:
+            print('Error! Map\'s metadata must include "width"')
+            exit()
+        if 'height' not in self.metadata:
+            print('Error! Map\'s metadata must include "height"')
+            exit()
 
     def load_map(self):
         self.map_data = []
         with open(map_filename, 'r') as fp:
             for line in fp:
                 line = line.strip()
+                if line[0] == '$':
+                    self.read_metadata(line)
+                    continue
                 tiles = []
                 for c in line: 
                     tiles.append(c)
@@ -68,7 +124,12 @@ class PatchHarvestAnimator:
         self.color_map['o'] = (100,100,100)
         self.color_map['E'] = (200,200,0)
         self.color_map['N'] = (0,150,200)
-        self.color_map['.'] = (200,200,200)
+        self.color_map['.'] = (150,150,150)
+        self.color_map['R'] = (200,50,0)
+        self.color_map['L'] = (0,50,200)
+        self.color_map['+'] = (255,255,255)
+        self.color_map['X'] = (50,200,50)
+        self.color_map['O'] = (200,200,0)
     
     def get_map_width(self):
         return len(self.map_data[0])
@@ -85,11 +146,22 @@ class PatchHarvestAnimator:
             self.map_data[self.agent.row_idx][self.agent.col_idx] = '.'
             print('Score:', self.agent.score)
 
+    def add_trace(self):
+        self.trace_points.append((self.agent.col_idx, self.agent.row_idx))
+
     def move(self):
         self.agent.move()
         self.agent.row_idx = max(min(self.agent.row_idx, self.get_map_height() - 1), 0)
         self.agent.col_idx = max(min(self.agent.col_idx, self.get_map_width() - 1), 0)
         self.consume()
+        self.add_trace()
+    
+    def move_back(self):
+        self.agent.move_back()
+        self.agent.row_idx = max(min(self.agent.row_idx, self.get_map_height() - 1), 0)
+        self.agent.col_idx = max(min(self.agent.col_idx, self.get_map_width() - 1), 0)
+        self.consume()
+        self.add_trace()
 
     def rotate_right(self): 
         self.agent.rotate_right()
@@ -104,12 +176,23 @@ class PatchHarvestAnimator:
         for row_idx in range(self.get_map_height()):
             for col_idx in range(self.get_map_width()):
                 col = self.get_color(row_idx, col_idx)
-                pygame.draw.rect(screen, (0,0,0), \
+                pygame.draw.rect(screen, col,\
                         (col_idx * tile_width, row_idx * tile_height, \
                             tile_width, tile_height))
-                pygame.draw.rect(screen, col, \
-                        (col_idx * tile_width + 1, row_idx * tile_height + 1, \
-                            tile_width - 1, tile_height - 1))
+                #pygame.draw.rect(screen, col, \
+                #        (col_idx * tile_width + 1, row_idx * tile_height + 1, \
+                #            tile_width - 1, tile_height - 1))
+        # Draw trace
+        for i in range(1, len(self.trace_points)):
+            x1, y1 = self.trace_points[i]
+            x2, y2 = self.trace_points[i-1]
+            x1 += 0.5
+            y1 += 0.5
+            x2 += 0.5
+            y2 += 0.5
+            color = (50 + (200 * i / len(self.trace_points)), 0, 0)
+            pygame.draw.line(screen, color, \
+                    (x1 * tile_width, y1 * tile_height), (x2 * tile_width, y2 * tile_height))
         # Draw agent
         surf = pygame.Surface((tile_width, tile_height))
         surf.set_colorkey((0,0,0))
@@ -156,7 +239,12 @@ if __name__ == '__main__':
     map_filename = sys.argv[1]
     movement_str = None
     if len(sys.argv) > 2:
-        movement_str = load_movement_file(sys.argv[2])
+        #movement_str = load_movement_file(sys.argv[2])
+        movement_str = sys.argv[2]
+    
+    output_filename = None
+    if len(sys.argv) > 3:
+        output_filename = sys.argv[3]
 
     animator = PatchHarvestAnimator(map_filename)
 
@@ -174,11 +262,24 @@ if __name__ == '__main__':
                 animator.rotate_left()
             elif movement_str[idx] == 'M':
                 animator.move()
-
+            elif movement_str[idx] == 'B':
+                animator.move_back()
     move_idx = 0
     playing = False
     time_delay = 0.01 
     done = False
+
+    # Trace, output, and stop if needed
+    if output_filename is not None:
+        while move_idx < len(movement_str):
+            process_move(move_idx)
+            move_idx += 1
+        animator.render(screen)
+        pygame.image.save(screen, output_filename)
+        print('Image saved to:', output_filename)
+        pygame.quit()
+        exit()
+
     while not done:
         for evt in pygame.event.get():
             if evt.type == pygame.QUIT:
